e8c0431e17bc7d750a13dabfe0352f5f4175b09f
Oleksandr Franchukkk <sha471251@gmail.com>
2023-11-01 19:27:30 +0200

    adaptiv img

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ export const browserSyncFunc = () => {
             baseDir: "docs"
         },
         open: true,
-        browser: "chrome"
+        browser: "brave"
         //port: 8080
     })
 }
